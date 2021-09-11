e90656fd53ffc76211aa6c277d01eb711b3265fc
jfulton007 <jfulton@csumb.edu>
2021-09-11 11:15:13 -0700

    Lab 4 done!

---
+++ lab4/js/lab.js
@@ -0,0 +1,10 @@
+// How to install a Steam game.
+  //Make sure you have Steam download & set up on your computer. You will also need a registered Steam account to complete this tutorial.
+  //Hover over the Library section of the Steam application, it is on the upper left, and then click on “Home”.
+  //Click on which game you’d like to install, then hit the bright blue “INSTALL” button.
+  //Grant the necessary permission to the program, wait for the download to complete, let the game install whatever software it needs - and you’re done! The game is ready to play.
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Joe Fulton.
+// @since  x.x.x
