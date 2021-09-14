71c8ffbb21a740110d0094bd6474335bff8398c6
jfulton007 <jfulton@csumb.edu>
2021-09-14 14:23:58 -0700

    Lab 5 done!

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+  * Author: Joe Fulton <jfulton@csumb.edu>
+  * Created: 13 September
+  * Liscense: Public Domain
+*/
+
+//Define Variables
+make = "Ford";
+model = "Fiesta";
+color = "Blue";
+year = 2011;
+
+//Calculate
+age = 2021 - year;
+
+//Output
+document.writeIn("Make: " + make + "<br>");
+document.writeIn("Model: " + model + "<br>");
+document.writeIn("Color: " + color + "<br>");
+document.writeIn("Year: " + year + "<br>");
+document.writeIn("Age: " + age + " years<br>");
