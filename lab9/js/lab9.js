a8826504d1e7462b2610bdbd3b3c079aab15a885
jfulton007 <jfulton@csumb.edu>
2021-10-03 14:26:53 -0700

    Lab 9 done!

---
+++ lab9/js/lab9.js
@@ -0,0 +1,8 @@
+/*
+  * Author: Joe Fulton <jfulton@csumb.edu>
+  * Created: 3 October
+  * Liscense: Public Domain
+*/
+getElementBlyd(div = "outputEl");
+document.createElement("button" = oneEl);
+document.createElement("button" = twoEl);
