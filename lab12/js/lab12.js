8f3b60a39cb509910b50e176b187c257dffd3b89
jfulton007 <jfulton@csumb.edu>
2021-10-12 13:54:28 -0700

    Lab 12 done!

---
+++ lab12/js/lab12.js
@@ -0,0 +1,14 @@
+/*
+  * Author: Joe Fulton <jfulton@csumb.edu>
+  * Created: 12 October
+  * Liscense: Public Domain
+*/
+$( "challenge-button" ).click(function() {
+  $( "this" ).slideUp();
+});
+$( "problem-button" ).click(function() {
+  $( "this" ).slideUp();
+});
+$( "result-button" ).click(function() {
+  $( "this" ).slideUp();
+});
--- lab12/js/lab8.js
+++
@@ -1,5 +0,0 @@
-/*
-  * Author: Joe Fulton <jfulton@csumb.edu>
-  * Created: 27 September
-  * Liscense: Public Domain
-*/
