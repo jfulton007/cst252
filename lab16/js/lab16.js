330f570e279db723e07906f76a64462c02e13eed
jfulton007 <jfulton@csumb.edu>
2021-11-03 10:54:18 -0700

    Update lab16.js

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -1,5 +1,18 @@
 /*
   * Author: Joe Fulton <jfulton@csumb.edu>
-  * Created: 27 September
+  * Created: 31 October
   * Liscense: Public Domain
 */
+function Vehicle(make, model, year, color, extras) {
+  this.makeName = Ford;
+  this.modelName = Fiesta;
+  this.yearName = 2011;
+  this.colorName = Blue;
+  this.extrasName = None;
+  this.info = function () {
+    return this.colorName + " " + this.yearName + " " + this.makeName + " " + this.modelName + " with " + this.extrasName + " called " + this.name;
+  }
+}
+const myCar = new Vehicle("Ford", "Fiesta", "2011", "Blue", "None");
+const myBike = new Vehicle("Basic", "Bike", "2009", "Black", "None");
+var bike = new Vehicle("Schwinn", "LeToure", 1974, "gold", "Brooks saddle");
