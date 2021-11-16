bef57aec0615291d2e0ccb01049397781b17a589
jfulton007 <jfulton@csumb.edu>
2021-11-16 15:58:28 -0800

    Lab 19 failed...

---
+++ lab19/js/lab19.js
@@ -0,0 +1,12 @@
+/*
+  * Author: Joe Fulton <jfulton@csumb.edu>
+  * Created: 16 November
+  * Liscense: Public Domain
+*/
+$.ajax({
+  url: "https://api.jquery.com/jquery.ajax/",
+  context: document.button
+})
+$( "button" ).click(function() {
+
+})
