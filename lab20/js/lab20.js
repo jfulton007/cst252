9c9676fdbd7b75d9b3699a6274c6f93dae2585d7
jfulton007 <jfulton@csumb.edu>
2021-11-30 15:17:58 -0800

    lab 20 failed...

---
+++ lab20/js/lab20.js
@@ -0,0 +1,9 @@
+/*
+  * Author: Joe Fulton <jfulton@csumb.edu>
+  * Created: 20 November
+  * Liscense: Public Domain
+*/
+var = newData = "Data";
+data.title = Title = "Lab 20 Comic";
+data.image = Image = "Comic Frame";
+data.alt = ALt = "https://xkcd.com/info.0.json";
